9e096624e833e83094b03d396d54047a279ccbae
rakotoshmu <rakotoshmu@users.noreply.github.com>
2015-06-03 17:41:29 +0200

    Update decomp.h

--- decomp.h
+++ decomp.h
@@ -151,7 +151,9 @@ printf("mu1 x nu1 : %d x %d\n",mu1,nu1);
 	x1[0]=0; x1[1]=w; x1[2]=w; x1[3]=0;
 	y1[0]=0; y1[1]=0; y1[2]=h; y1[3]=h;
 
-	//x2,y2 = AA(x1,y1) où AA = A^(-1)
+	//On calcule la positions des coins des images intermediaire (x2,y2 puis x3,y3)
+
+	//AA = A^(-1)
 	double det_A = A[0]*A[4]-A[3]*A[1];
         double AA[6] = {
             A[4]/det_A,
@@ -161,14 +163,14 @@ printf("mu1 x nu1 : %d x %d\n",mu1,nu1);
             A[0]/det_A,
             (-A[0]*A[5]+A[3]*A[2])/det_A
         };
-
-	//On precacul les coins des images intermediaire 
+        //x2,y2 = AA(x1,y1) : img2 sera l'image issue d'une rotation-translation de l'image d'entrée (img2 = A^(-1)(img1))
         double x2[4];
         double y2[4];
         for(int k=0;k<4;k++){
             x2[k]=x1[k]*AA[0]+y1[k]*AA[1]+AA[2];
             y2[k]=x1[k]*AA[3]+y1[k]*AA[4]+AA[5];
         }
+        //on encadre ces 4 coins dans un rectangle d'origine (mu2,nu2) et de taille w2*h2
         double temp_min_x2 = x2[0];
         double temp_min_y2 = y2[0];
         double temp_max_x2 = x2[0];
@@ -188,22 +190,19 @@ printf("mu1 x nu1 : %d x %d\n",mu1,nu1);
         //augmenter w2, h2 pour avoir même parité que w1, h1
         if((w2-w1)%2!=0){w2++;}
         if((h2-h1)%2!=0){h2++;}
-/* pour contraindre la taille de img2
-int ns2 = 512; //ns la new size
-mu2 = mu2+(w2-ns2)/2; nu2 = nu2+(h2-ns2)/2;
-w2 = ns2; h2 = ns2;*/
 printf("w2 x h2 : %d x %d\n",w2,h2);
 printf("mu2 x nu2 : %d x %d\n",mu2,nu2);
 
 
 
-        //x3,y3 = B(x4,y4)
+        //x3,y3 = B(x4,y4) : img3 sera l'image qui, par une rotation, sera transformée en l'image de sortie (img4 = B^(-1)(img3))
         double x3[4];
         double y3[4];
         for(int k=0;k<4;k++){
             x3[k]=x4[k]*B[0]+y4[k]*B[1]+B[2];
             y3[k]=x4[k]*B[3]+y4[k]*B[4]+B[5];
         }
+        //on encadre ces 4 coins dans un rectangle d'origine (mu3,nu3) et de taille w3*h3
         double temp_min_x3 = x3[0];
         double temp_min_y3 = y3[0];
         double temp_max_x3 = x3[0];
@@ -222,16 +221,11 @@ printf("mu2 x nu2 : %d x %d\n",mu2,nu2);
         //augmenter w3, h3 pour avoir même parité que w_f, h_f
         if((w3-w_f)%2!=0){w3++;}
         if((h3-h_f)%2!=0){h3++;}
-/* pour contraindre la taille de img3
-int ns3 = 512; //ns la new size
-mu3 = mu3+(w3-ns3)/2; nu3 = nu3+(h3-ns3)/2;
-w3 = ns3; h3 = ns3;
-*/
 printf("w3 x h3 : %d x %d\n",w3,h3);
 printf("mu3 x nu3 : %d x %d\n",mu3,nu3);
 
 
-
+	//la dernière image intermédiaire sera l'image de sortie
         int w4 = w_f, h4 = h_f;
         int mu4 = 0, nu4 = 0;
 printf("w4 x h4 : %d x %d\n",w4,h4);
@@ -239,111 +233,45 @@ printf("mu4 x nu4 : %d x %d\n\n",mu4,nu4);
 
 
 
-        //à ce stade, on a pris en compte une grande partie des translations de A et , via les mu,nu
+        //à ce stade, on a pris en compte une grande partie des translations, via les mu,nu
         //on modifie donc A et B en conséquence
 
         A[2] = mu2*A[0] + nu2*A[1] + A[2] - mu1;
         A[5] = mu2*A[3] + nu2*A[4] + A[5] - nu1;
         B[2] = mu4*B[0] + nu4*B[1] + B[2] - mu3;
         B[5] = mu4*B[3] + nu4*B[4] + B[5] - nu3;
-
-
-
-        /** allers et retours de rotation (A, A^(-1), A, A^(-1), ...)
-        float *img1 = malloc(3*w1*h1*sizeof(float));
-        for(int i=0;i<3*w1*h1;i++){img1[i]=img[i];}
-
-        float *img2 = malloc(3*w2*h2*sizeof(float));
-        float *img4 = malloc(3*w4*h4*sizeof(float));
-
-
-printf("------------------------ 0/8\n");
-        apply_affinite(img1,img2,w1,h1,w2,h2,A);
-printf("+++--------------------- 1/8\n");
-        apply_affinite(img2,img4,w2,h2,w4,h4,B);
-printf("++++++------------------ 2/8\n");
-        apply_affinite(img4,img2,w4,h4,w2,h2,A);
-printf("+++++++++--------------- 3/8\n");
-        apply_affinite(img2,img4,w2,h2,w4,h4,B);
-printf("++++++++++++------------ 4/8\n");
-        apply_affinite(img4,img2,w4,h4,w2,h2,A);
-printf("+++++++++++++++--------- 5/8\n");
-        apply_affinite(img2,img4,w2,h2,w4,h4,B);
-printf("++++++++++++++++++------ 6/8\n");
-        apply_affinite(img4,img2,w4,h4,w2,h2,A);
-printf("+++++++++++++++++++++--- 7/8\n");
-        apply_affinite(img2,img4,w2,h2,w4,h4,B);
-printf("++++++++++++++++++++++++ 8/8\n");
-
-        for(int l=0;l<3;l++){
-            for(int i=0;i<w_f;i++){
-                for(int j=0;j<h_f;j++){
-                    img_f[(i+w_f*j)*3+l]=img4[(i+w4*j)*3+l];
-                }
-            }
-        }
-        //*/
-        /** 6 rotations identiques
-        w2 = 2*w1; h2 = 2*h1;
-        w3 = w2; h3 = h2;
-        float *img1 = malloc(3*w1*h1*sizeof(float));
-        for(int i=0;i<3*w1*h1;i++){img1[i]=img[i];}
-
-        float *img2 = malloc(3*w2*h2*sizeof(float));
-        float *img4 = malloc(3*w4*h4*sizeof(float));
-        float *img3 = malloc(3*w3*h3*sizeof(float));
-
-printf("------------------------------ 0/6\n");
-        apply_affinite(img1,img2,w1,h1,w2,h2,A);
-printf("+++++------------------------- 1/6\n");
-        apply_affinite(img2,img3,w2,h2,w3,h3,A);
-printf("++++++++++-------------------- 2/6\n");
-        apply_affinite(img3,img2,w3,h3,w2,h2,A);
-printf("+++++++++++++++--------------- 3/6\n");
-        apply_affinite(img2,img3,w2,h2,w3,h3,A);
-printf("++++++++++++++++++++---------- 4/6\n");
-        apply_affinite(img3,img2,w3,h3,w2,h2,A);
-printf("+++++++++++++++++++++++++----- 5/6\n");
-        apply_affinite(img2,img4,w2,h2,w4,h4,A);
-printf("++++++++++++++++++++++++++++++ 6/6\n");
-
-        for(int l=0;l<3;l++){
-            for(int i=0;i<w_f;i++){
-                for(int j=0;j<h_f;j++){
-                    img_f[(i+w_f*j)*3+l]=img4[(i+w4*j)*3+l];
-                }
-            }
-        }
-        //*/
-        ///** code original (application de la décomposition)
+        
+        
+        
 printf("\ntransformation de l'image en cours\n");
         float *img1 = malloc(3*w1*h1*sizeof(float));
-		for(int i=0;i<3*w1*h1;i++){img1[i]=img[i];}
+	for(int i=0;i<3*w1*h1;i++){img1[i]=img[i];}
 
         float *img2 = malloc(3*w2*h2*sizeof(float));
-		apply_affinite(img1,img2,w1,h1,w2,h2,A);
-		//free(img1);
+	apply_affinite(img1,img2,w1,h1,w2,h2,A);
+	//free(img1);
 printf("application de A  : done\n");
 
-		float *img3 = malloc(3*w3*h3*sizeof(float));
-		apply_homo(img2,img3,w2,h2,w3,h3,mu2,nu2,mu3,nu3,H0);
-		//free(img2);
+	float *img3 = malloc(3*w3*h3*sizeof(float));
+	apply_homo(img2,img3,w2,h2,w3,h3,mu2,nu2,mu3,nu3,H0);
+	//free(img2);
 printf("application de H0 : done\n");
 
-		float *img4 = malloc(3*w_f*h_f*sizeof(float));
-		apply_affinite(img3,img4,w3,h3,w4,h4,B);
-		//free(img3);
+	float *img4 = malloc(3*w_f*h_f*sizeof(float));
+	apply_affinite(img3,img4,w3,h3,w4,h4,B);
+	//free(img3);
 printf("application de B  : done\n");
 
-		for(int l=0;l<3;l++)
+	for(int l=0;l<3;l++)
             for(int i=0;i<w_f;i++)
                 for(int j=0;j<h_f;j++)
                     {img_f[(i+w_f*j)*3+l]=img4[(i+w4*j)*3+l];}
         //free(img4);
-        //*/
 	}
-	
-	//on tronque la zone du a la symetrisation (dans "affine.h", pour éviter l'effet gibbs)
+
+
+
+	//on tronque la zone due a la symetrisation (dans "affine.h", pour éviter l'effet gibbs)
 	double p[2];
 	for(int i=0;i<w_f;i++){
 		for(int j=0;j<h_f;j++){
